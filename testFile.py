60622220f77373c510e40c2408ecc650da0a63b5
Ishan Madhu <ishan_009@outlook.com>
2022-12-07 19:02:59 -0600

    check the basics of python

--- testFile.py
+++ testFile.py
@@ -1,1 +1,18 @@
-print('Hello there')
+from operator import add, mul
+from math import pi
+
+print(mul(2,3))
+
+## Structure of call expression
+# Operator ( Operand, Operand )
+# Operator and operands are also expression to evaluate.
+
+print(mul(add(2,mul(4,6)),add(3,5)))
+
+rad = 5.0
+area,radius = pi * pow(rad,2.0), 2 * pi * rad
+print(area,radius)
+
+f = max
+print(f(1,3,2))
+
